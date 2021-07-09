6b21696fce70c271b12703caaabb7dfce15c828c
Anchit Verma <av.anchit@yahoo.com>
2021-07-10 02:20:09 +1000

    added i3 config

---
+++ .config/i3/decrease-brightness.sh
@@ -0,0 +1,1 @@
+busctl call org.clightd.clightd /org/clightd/clightd/Backlight org.clightd.clightd.Backlight LowerAll "d(bdu)s" 0.05 0 0 0 ""
---
+++ .config/i3/increase-brightness.sh
@@ -0,0 +1,1 @@
+busctl call org.clightd.clightd /org/clightd/clightd/Backlight org.clightd.clightd.Backlight RaiseAll "d(bdu)s" 0.05 0 0 0 ""
